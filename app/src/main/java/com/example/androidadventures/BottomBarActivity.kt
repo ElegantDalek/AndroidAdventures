92cb10ce7cadf6aaf92b49650a55f4e1867012d7
Theodore Li <theodoreqili@gmail.com>
2019-11-01 20:01:40 -0500

    Create view pager

--- app/src/main/java/com/example/androidadventures/BottomBarActivity.kt
+++ app/src/main/java/com/example/androidadventures/BottomBarActivity.kt
@@ -1,12 +1,21 @@
 package com.example.androidadventures
 
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 
-class BottomBarActivity : AppCompatActivity() {
+class BottomBarActivity : AppCompatActivity(), BigCards.OnFragmentInteractionListener {
+    override fun onFragmentInteraction(uri: Uri) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_bottom_bar)
+
+        val fragmentTransaction = supportFragmentManager.beginTransaction()
+        val newFragment: Fragment = BigCards()
+        fragmentTransaction.add(R.id.fragment_container, newFragment).commit()
     }
 }
